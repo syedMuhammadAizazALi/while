13382e67e8f7fc84959c2b1917ca5ce826ef607e
Syed Muhammad Aizaz Ali <136581038+syedMuhammadAizazALi@users.noreply.github.com>
2023-08-25 11:00:18 -0700

    done

---
+++ app.js
@@ -0,0 +1,32 @@
+// let div = document.querySelector("div");
+// for(i=1;i<=10; i++){
+//     console.log();
+//     div.innerHTML+=`<br/>`
+//     for(j=1;j<=10; j++){
+//         div.innerHTML+=`${i} * ${j} = ${i*j} <br/>`;
+    
+//     }
+// }
+
+// let div = document.querySelector("div");
+// // let num = prompt("put any value")
+//  i = 1
+//  j= 1
+// while(i<=10){
+//    console.log(i);
+// //  div.innerHTML+=`${i} * ${j} = ${i*j} <br/> `
+//     i++
+//     while(j<=10){
+//         div.innerHTML+=`${i} * ${j} = ${i*j} <br/> `
+//         j++
+        
+//     }
+// }
+
+number = 5 
+count = 1  
+
+while count <= 10:
+    result = number * count
+    print("{number} x {count} = {result}")
+    count += 1
